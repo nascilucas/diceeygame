9617ad2c54b617abb9f4083a4e4665e593f7d851
nascilucas <40007491+nascilucas@users.noreply.github.com>
2022-01-21 15:38:40 -0600

    out with the switch

--- files/index.js
+++ files/index.js
@@ -1,52 +1,14 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var diceQueryPlayer1 = document.querySelector(".player1");
+var diceQueryPlayer1 = document.querySelectorAll(".playerDice")[0];
+var diceImagePlayer1 = "images/dice" + randomNumber1 + ".png";
+diceQueryPlayer1.setAttribute("src", diceImagePlayer1);
 
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-var diceQueryPlayer2 = document.querySelector(".player2");
+var diceQueryPlayer2 = document.querySelectorAll(".playerDice")[1];
+var diceImagePlayer2 = "images/dice" + randomNumber2 + ".png";
+diceQueryPlayer2.setAttribute("src", diceImagePlayer2);
 
 var titleSelector = document.querySelector(".dyn-header h1");
-
-switch (randomNumber1) {
-  case 1:
-    diceQueryPlayer1.setAttribute("src", "images/dice1.png");
-    break;
-  case 2:
-    diceQueryPlayer1.setAttribute("src", "images/dice2.png");
-    break;
-  case 3:
-    diceQueryPlayer1.setAttribute("src", "images/dice3.png");
-    break;
-  case 4:
-    diceQueryPlayer1.setAttribute("src", "images/dice4.png");
-    break;
-  case 5:
-    diceQueryPlayer1.setAttribute("src", "images/dice5.png");
-    break;
-  case 6:
-    diceQueryPlayer1.setAttribute("src", "images/dice6.png");
-    break;
-}
-
-switch (randomNumber2) {
-  case 1:
-    diceQueryPlayer2.setAttribute("src", "images/dice1.png");
-    break;
-  case 2:
-    diceQueryPlayer2.setAttribute("src", "images/dice2.png");
-    break;
-  case 3:
-    diceQueryPlayer2.setAttribute("src", "images/dice3.png");
-    break;
-  case 4:
-    diceQueryPlayer2.setAttribute("src", "images/dice4.png");
-    break;
-  case 5:
-    diceQueryPlayer2.setAttribute("src", "images/dice5.png");
-    break;
-  case 6:
-    diceQueryPlayer2.setAttribute("src", "images/dice6.png");
-    break;
-}
 
 if (randomNumber1 === randomNumber2) {
   titleSelector.textContent = "Draw 🥱";
